10d2b74e4395a00c533405c820ed265fd09061bf
Keith <mcis6183@gmail.com>
2016-04-05 20:51:50 -0600

    initial implementation

---
+++ EXAMPLES/example_extractor.py
@@ -0,0 +1,26 @@
+import sys
+import random
+
+def process_input_file(filename, output_size=10, variables=["VS010"], random=False):
+    with open(filename) as fi:
+        variables_indices = []
+
+        for line_number, line in enumerate(fi):
+            line_data = line.split()
+            if line_number == 0: # process the header -> get the index # of the variables
+                for v in variables:
+                    variables_indices.append(line_data.index(v))
+                    print "{}\t".format(v),
+                print
+            elif line_number < output_size+1:
+                for v in variables_indices:
+                    print "{}\t".format(line_data[v]),
+                print
+
+
+def main():
+    fname = "C:\\Users\\Keith\\Downloads\\26362-0001-Data.tsv"
+    process_input_file(fname, variables=["V3079","VS002","VS012","VS013","VS014","VS015","VS017","VS019"], output_size=500)
+
+if __name__ == "__main__":
+    main()
